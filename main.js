b830912fbbb7d344dde94779a190792e3186c2a4
giorgio laera <laeragiorg@gmail.com>
2025-11-12 20:27:54 +0100

    ticket

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const button = document.getElementById('bottone');
+const image =document.getElementById('img');
+
+image.src = './img/yellow_lamp.png';
+button.innerText = "spegni la lampadina";
+
