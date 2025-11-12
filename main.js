fbd906ed1286091f421f54911f99c08eeca376f7
giorgio laera <laeragiorg@gmail.com>
2025-11-12 20:41:12 +0100

    complete

--- main.js
+++ main.js
@@ -2,6 +2,17 @@
 const button = document.getElementById('bottone');
 const image =document.getElementById('img');
 
-image.src = './img/yellow_lamp.png';
-button.innerText = "spegni la lampadina";
+// image.src = './img/yellow_lamp.png';
+// button.innerText = "spegni la lampadina";
 
+button.addEventListener("click",function(){
+    console.log("bottone cliccato",image.src)
+
+    if(image.src.includes("white_lamp.png")){
+        image.src = './img/yellow_lamp.png';
+        button.innerText = "spegni la lampadina";
+    }else{
+        image.src = './img/white_lamp.png';
+        button.innerText = "accendi la lampadina";
+    }
+})
